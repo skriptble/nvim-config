d3a1a1aa0733f572148c3ec3ed5f3a2270a0d367
Kris Brandow <kris.brandow@gmail.com>
2026-03-18 14:51:15 -0400

    Update treesitter and related plugins
    They were on the master branch, using the legacy configuration format.
    They've been updated for the new main branch and the new configuration
    format.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,110 +1,99 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    branch = "master",
+    branch = "main",
+    lazy = false,
     build = ":TSUpdate",
-    dependencies = {
-        { "nvim-treesitter/nvim-treesitter-textobjects", branch = "master" },
-    },
-    opts = {
-      -- Add languages to be installed here that you want installed for treesitter
-      ensure_installed = {
-          'c', 'go', 'lua', 'rust', 'typescript', 'html', 'javascript', 'css',
-          'vimdoc', 'vim', 'query', 'glsl',
-      },
+    config = function()
+      -- Register custom parsers
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "TSUpdate",
+        callback = function()
+          ---@diagnostic disable: inject-field, missing-fields
+          require("nvim-treesitter.parsers").templ = {
+            install_info = {
+              url = "https://github.com/vrischmann/tree-sitter-templ.git",
+              files = { "src/parser.c", "src/scanner.c" },
+              branch = "master",
+            },
+          }
+          require("nvim-treesitter.parsers").d2 = {
+            install_info = {
+              url = "https://git.pleshevski.ru/pleshevskiy/tree-sitter-d2",
+              revision = "main",
+              files = { "src/parser.c", "src/scanner.c" },
+            },
+          }
+        end,
+      })
 
-      highlight = { enable = true },
-      indent = { enable = false },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = '<c-space>',
-          node_incremental = '<c-space>',
-          scope_incremental = '<c-s>',
-          node_decremental = '<c-backspace>',
-        },
-      },
-      textobjects = {
-        select = {
-          enable = true,
-          lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-          keymaps = {
-            -- You can use the capture groups defined in textobjects.scm
-            ['aa'] = '@parameter.outer',
-            ['ia'] = '@parameter.inner',
-            ['af'] = '@function.outer',
-            ['if'] = '@function.inner',
-            ['ac'] = '@class.outer',
-            ['ic'] = '@class.inner',
-          },
-        },
-        move = {
-          enable = true,
-          set_jumps = true, -- whether to set jumps in the jumplist
-          goto_next_start = {
-            [']m'] = '@function.outer',
-            [']]'] = '@class.outer',
-          },
-          goto_next_end = {
-            [']M'] = '@function.outer',
-            [']['] = '@class.outer',
-          },
-          goto_previous_start = {
-            ['[m'] = '@function.outer',
-            ['[['] = '@class.outer',
-          },
-          goto_previous_end = {
-            ['[M'] = '@function.outer',
-            ['[]'] = '@class.outer',
-          },
-        },
-        swap = {
-          enable = true,
-          swap_next = {
-            ['<leader>a'] = '@parameter.inner',
-          },
-          swap_previous = {
-            ['<leader>A'] = '@parameter.inner',
-          },
-        },
-      },
-    },
-    config = function(_, opts)
-        ---@diagnostic disable: inject-field
-        local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-        parser_config.templ = {
-          install_info = {
-            url = "https://github.com/vrischmann/tree-sitter-templ.git",
-            files = { "src/parser.c", "src/scanner.c" },
-            branch = "master",
-          },
-        }
+      vim.treesitter.language.register("templ", "templ")
 
-        vim.treesitter.language.register('templ', 'templ')
-
-        parser_config.d2 = {
-          install_info = {
-            url = "https://git.pleshevski.ru/pleshevskiy/tree-sitter-d2",
-            revision = "main",
-            files = { "src/parser.c", "src/scanner.cc" },
-          },
-          filetype = "d2",
-        }
-
-        require("nvim-treesitter.configs").setup(opts)
-
-        vim.api.nvim_create_augroup('TSJS', { clear = true })
-        vim.api.nvim_create_autocmd('FileType', {
-          group = 'TSJS',
-          pattern = { 'javascript' },
-          callback = function(ev)
-            require('nvim-treesitter.configs').commands.TSBufEnable.run("indent", ev.bufnr, "javascript")
-          end,
+      -- Install parser (no-op if already installed)
+      local ok, ts = pcall(require, "nvim-treesitter")
+      if ok and ts.install then
+        require("nvim-treesitter").install({
+          "c", "go", "lua", "rust", "typescript", "html",
+          "javascript", "css", "vimdoc", "vim", "query", "glsl",
         })
+      end
+
+      -- Enable treesitter highlighting for all filetypes
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function(args)
+          pcall(vim.treesitter.start, args.buf)
+        end,
+      })
+
+      -- Enable treesitter indentation for JavaScript
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "javascript" },
+        callback = function()
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
+      })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    branch = "main",
+    config = function()
+      require("nvim-treesitter-textobjects").setup({
+        select = {
+          lookahead = true,
+        },
+      })
+
+      -- Select textobjects
+      local select_fn = require("nvim-treesitter-textobjects.select")
+      for _, mode in ipairs({ "x", "o" }) do
+        vim.keymap.set(mode, "aa", function() select_fn.select_textobject("@parameter.outer", "textobjects") end, { desc = "Select outer parameter" })
+        vim.keymap.set(mode, "ia", function() select_fn.select_textobject("@parameter.inner", "textobjects") end, { desc = "Select inner parameter" })
+        vim.keymap.set(mode, "af", function() select_fn.select_textobject("@function.outer", "textobjects") end, { desc = "Select outer function" })
+        vim.keymap.set(mode, "if", function() select_fn.select_textobject("@function.inner", "textobjects") end, { desc = "Select inner function" })
+        vim.keymap.set(mode, "ac", function() select_fn.select_textobject("@class.outer", "textobjects") end, { desc = "Select outer class" })
+        vim.keymap.set(mode, "ic", function() select_fn.select_textobject("@class.inner", "textobjects") end, { desc = "Select inner class" })
+      end
+
+      -- Move between textobjects
+      local move = require("nvim-treesitter-textobjects.move")
+      vim.keymap.set({ "n", "x", "o" }, "]m", function() move.goto_next_start("@function.outer", "textobjects") end, { desc = "Next function start" })
+      vim.keymap.set({ "n", "x", "o" }, "]]", function() move.goto_next_start("@class.outer", "textobjects") end, { desc = "Next class start" })
+      vim.keymap.set({ "n", "x", "o" }, "]M", function() move.goto_next_end("@function.outer", "textobjects") end, { desc = "Next function end" })
+      vim.keymap.set({ "n", "x", "o" }, "][", function() move.goto_next_end("@class.outer", "textobjects") end, { desc = "Next class end" })
+      vim.keymap.set({ "n", "x", "o" }, "[m", function() move.goto_previous_start("@function.outer", "textobjects") end, { desc = "Previous function start" })
+      vim.keymap.set({ "n", "x", "o" }, "[[", function() move.goto_previous_start("@class.outer", "textobjects") end, { desc = "Previous class start" })
+      vim.keymap.set({ "n", "x", "o" }, "[M", function() move.goto_previous_end("@function.outer", "textobjects") end, { desc = "Previous function end" })
+      vim.keymap.set({ "n", "x", "o" }, "[]", function() move.goto_previous_end("@class.outer", "textobjects") end, { desc = "Previous class end" })
+
+      -- Swap parameters
+      local swap = require("nvim-treesitter-textobjects.swap")
+      vim.keymap.set("n", "<leader>a", function() swap.swap_next("@parameter.inner", "textobjects") end, { desc = "Swap next parameter" })
+      vim.keymap.set("n", "<leader>A", function() swap.swap_previous("@parameter.inner", "textobjects") end, { desc = "Swap previous parameter" })
     end,
   },
   {
     "nvim-treesitter/nvim-treesitter-context",
-    branch = "master",
+    opts = {},
   },
 }
